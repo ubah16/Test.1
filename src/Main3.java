d40ea74d42dfeead0da85f4b60c375c1a7a2922e
ubahmohamoud <ubahmohamoud22@gmail.com>
2026-09-04 18:02:08 +0200

    Lägg till Java- program

--- src/Main3.java
+++ src/Main3.java
@@ -1,1 +1,14 @@
+public class Main3 {
+    public static void main (String [] args) {
+        String[] studentNames = {"Anna", "Leo", "Sara", "Noah"};
+        printNames(studentNames);
 
+
+    }
+
+public static void printNames (String [] names) {
+    for (int index = 0; index < names.length; index++) {
+        System.out.println(names[index]);
+    }
+}
+}
