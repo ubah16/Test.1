ddba0aa48a49098070d689c189429a622ae202b8
ubahmohamoud <ubahmohamoud22@gmail.com>
2026-09-04 16:33:06 +0200

    initial commit

--- src/Main2.java
+++ src/Main2.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Main2 {
+    static void main () {
+
+        printCourseInfo();
+        greetStudent("Ubah");
+        int summa = calculateTotal (4, 9);
+
+        System.out.println(summa);
+
+
+
+    }
+
+    public static void printCourseInfo() {
+        System.out.println("Welcome to the course, today we are in the classroom 5");
+    }
+
+    public static void greetStudent(String name){
+        System.out.println("Hello " + name);
+    }
+    public static int calculateTotal(int firstNumber, int secondNumber){
+       return firstNumber + secondNumber;
+
+    }
 }
--- src/Main4.java
+++ src/Main4.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Main4 {
+    public static void main(String[] args) {
+        int[] favouriteNumbers2 = {1, 3, 6, 8, 9};
+
+
+        for (int index = 0; index < favouriteNumbers2.length; index++) {
+            System.out.println(favouriteNumbers2[index]);
+        }
+
+        int summa = 0;
+        for (int index = 0; index < favouriteNumbers2.length; index++) {
+            summa += favouriteNumbers2[index];
+        }
+
+        System.out.println("Summan är: " + summa);
+    }
 }
